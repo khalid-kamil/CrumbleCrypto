4695abec335a76c4bbcbf803a4de4099aadc8ffe
Khalid Kamil <khalidkamil28@gmail.com>
2023-12-20 17:50:49 +0000

    Initial Commit

---
+++ CrumbleCrypto/CrumbleCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrumbleCryptoApp.swift
+//  CrumbleCrypto
+//
+//  Created by Khalid Kamil on 20/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct CrumbleCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
