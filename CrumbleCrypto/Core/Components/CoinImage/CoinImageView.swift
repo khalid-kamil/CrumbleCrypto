d917a45fd1e7d53fe696eb621e40df24999de1c7
Khalid Kamil <khalidkamil28@gmail.com>
2023-12-21 17:03:36 +0000

    Download coin images using `CoinImageService` networking layer and Combine

---
+++ CrumbleCrypto/Core/Components/CoinImage/CoinImageView.swift
@@ -0,0 +1,37 @@
+//
+//  CoinImageView.swift
+//  CrumbleCrypto
+//
+//  Created by Khalid Kamil on 21/12/2023.
+//
+
+import SwiftUI
+
+struct CoinImageView: View {
+    
+    @StateObject var vm: CoinImageViewModel
+
+    init(coin: Coin) {
+        _vm = StateObject(wrappedValue: CoinImageViewModel(coin: coin))
+    }
+
+    var body: some View {
+        ZStack {
+            if let image = vm.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+            } else if vm.isLoading {
+                ProgressView()
+            } else {
+                Image(systemName: "questionmark")
+                    .foregroundStyle(Color.theme.secondaryText)
+            }
+        }
+    }
+}
+
+#Preview("Coin Image", traits: .sizeThatFitsLayout) {
+    CoinImageView(coin: PreviewContent.sample.coin)
+        .padding()
+}
---
+++ CrumbleCrypto/Core/Components/CoinImage/CoinImageViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  CoinImageViewModel.swift
+//  CrumbleCrypto
+//
+//  Created by Khalid Kamil on 21/12/2023.
+//
+
+import Combine
+import SwiftUI
+
+class CoinImageViewModel: ObservableObject {
+    @Published var image: UIImage? = nil
+    @Published var isLoading: Bool = false
+
+    private let coin: Coin
+    private let dataService: CoinImageService
+    private var cancellables = Set<AnyCancellable>()
+
+    init(coin: Coin) {
+        self.coin = coin
+        self.dataService = CoinImageService(urlString: coin.image)
+        self.addSubscribers()
+        self.isLoading = true
+    }
+
+    private func addSubscribers() {
+        dataService.$image
+            .sink { (_) in
+                self.isLoading = false
+            } receiveValue: { [weak self] (returnedImage) in
+                self?.image = returnedImage
+            }
+            .store(in: &cancellables)
+    }
+}
