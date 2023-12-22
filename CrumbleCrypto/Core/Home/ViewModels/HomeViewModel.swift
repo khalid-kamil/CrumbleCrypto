013dc41349bfc41161687ccc25aacb7dcd5dc483
Khalid Kamil <khalidkamil28@gmail.com>
2023-12-22 09:44:04 +0000

    Filtering data based on search bar text using Combine

--- CrumbleCrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ CrumbleCrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -22,10 +22,29 @@ class HomeViewModel: ObservableObject {
     }
 
     func addSubscribers() {
-        dataService.$allCoins
+        // updates allCoins
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filterCoins)
             .sink { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
+    }
+
+
+    private func filterCoins(text: String, startingCoins: [Coin]) -> [Coin] {
+        guard !text.isEmpty else {
+            return startingCoins
+        }
+
+        let lowercasedText = text.lowercased()
+
+        return startingCoins.filter { (coin) -> Bool in
+            return coin.name.lowercased().contains(lowercasedText) ||
+            coin.symbol.lowercased().contains(lowercasedText) ||
+            coin.id.lowercased().contains(lowercasedText)
+        }
     }
 }
