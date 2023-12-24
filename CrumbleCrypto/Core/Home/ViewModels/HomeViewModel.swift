c53a202846d5175c001ed400e6ebf45414674eb6
Khalid Kamil <khalidkamil28@gmail.com>
2023-12-24 22:50:23 +0000

    Sorting List data with MVVM and animations

--- CrumbleCrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ CrumbleCrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -10,8 +10,6 @@ import Foundation
 
 class HomeViewModel: ObservableObject {
 
-
-
     @Published var statistics: [Statistic] = [
         Statistic(title: "Title", value: "Value", percentageChange: 1),
         Statistic(title: "Title", value: "Value"),
@@ -23,11 +21,16 @@ class HomeViewModel: ObservableObject {
     @Published var portfolioCoins: [Coin] = []
     @Published var isLoading: Bool = false
     @Published var searchText: String = ""
+    @Published var sortOption: SortOption = .holdings
 
     private let coinDataService = CoinDataService()
     private let marketDataService = MarketDataService()
     private let portfolioDataService = PortfolioDataService()
     private var cancellables = Set<AnyCancellable>()
+
+    enum SortOption {
+        case rank, rankReversed, holdings, holdingsReversed, price, priceReversed
+    }
 
     init() {
         addSubscribers()
@@ -36,9 +39,9 @@ class HomeViewModel: ObservableObject {
     func addSubscribers() {
         // updates allCoins
         $searchText
-            .combineLatest(coinDataService.$allCoins)
+            .combineLatest(coinDataService.$allCoins, $sortOption)
             .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
-            .map(filterCoins)
+            .map(filterAndSortCoins)
             .sink { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
             }
@@ -49,7 +52,8 @@ class HomeViewModel: ObservableObject {
             .combineLatest(portfolioDataService.$savedEntities)
             .map(mapAllCoinsToPortfolioCoins)
             .sink { [weak self] (returnedCoins) in
-                self?.portfolioCoins = returnedCoins
+                guard let self = self else { return }
+                self.portfolioCoins = self.sortPortfolioCoinsIfNeeded(coins: returnedCoins)
             }
             .store(in: &cancellables)
 
@@ -62,8 +66,6 @@ class HomeViewModel: ObservableObject {
                 self?.isLoading = false
             }
             .store(in: &cancellables)
-
-
     }
 
     func updatePortfolio(coin: Coin, amount: Double) {
@@ -77,15 +79,45 @@ class HomeViewModel: ObservableObject {
         HapticManager.notification(type: .success)
     }
 
+    private func filterAndSortCoins(text: String, coins: [Coin], sort: SortOption) -> [Coin] {
+        var updatedCoins = filterCoins(text: text, coins: coins)
+        sortCoins(sort: sort, coins: &updatedCoins)
+        return updatedCoins
+    }
 
-    private func filterCoins(text: String, startingCoins: [Coin]) -> [Coin] {
+    private func sortCoins(sort: SortOption, coins: inout [Coin]) {
+        switch sort {
+        case .rank, .holdings:
+            coins.sort(by: { $0.rank < $1.rank })
+        case .rankReversed, .holdingsReversed:
+            coins.sort(by: { $0.rank > $1.rank })
+        case .price:
+            coins.sort(by: { $0.currentPrice < $1.currentPrice })
+        case .priceReversed:
+            coins.sort(by: { $0.currentPrice > $1.currentPrice })
+        }
+    }
+
+    private func sortPortfolioCoinsIfNeeded(coins: [Coin]) -> [Coin] {
+        // will only sort by holdings or reversedHoldings if needed
+        switch sortOption {
+        case .holdings:
+            return coins.sorted(by: { $0.currentHoldingsValue > $1.currentHoldingsValue })
+        case .holdingsReversed:
+            return coins.sorted(by: { $0.currentHoldingsValue < $1.currentHoldingsValue })
+        default:
+            return coins
+        }
+    }
+
+    private func filterCoins(text: String, coins: [Coin]) -> [Coin] {
         guard !text.isEmpty else {
-            return startingCoins
+            return coins
         }
 
         let lowercasedText = text.lowercased()
 
-        return startingCoins.filter { (coin) -> Bool in
+        return coins.filter { (coin) -> Bool in
             return coin.name.lowercased().contains(lowercasedText) ||
             coin.symbol.lowercased().contains(lowercasedText) ||
             coin.id.lowercased().contains(lowercasedText)
